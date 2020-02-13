fd1022ecf92a81d13fe5d0f6e22ab8fa1b408ab3
Avinash <59113057+Viwarrior@users.noreply.github.com>
2020-02-13 21:45:22 +0530

    For finding best para
    I have iterated over all para, this will take a lot more time. But in reality everytime  we will add new text in database, we will assign new score to it based on top m(for us =10) mistakes. Then the scores of all the texts will be updated periodically based on the mistake list ( or when user is not racing).

---
+++ algotochoose.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 16:00:00 2020
+@name: Algorithm to choose best paragraph from practice mode
+@author: AVINASH
+"""
+#saari kaam ki nhi hai dar mat
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import nltk
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from nltk import sent_tokenize
+
+# user defined functions
+#term frequency calculation
+def tf(word, parat):
+    c = parat.count(word)
+    return(c/len(parat))
+#tokenize the para, removing stop words like 'the, is' etc..and symbols.
+def tokenpara(para):
+    tokenizer = RegexpTokenizer(r'\w+')
+    parat = tokenizer.tokenize(para)
+    stop_words = set(stopwords.words('english'))
+    parat = [w for w in parat if not w in stop_words]
+    return(parat)
+    
+# a sample representing user top mistakes
+mistakes = ['open','rubbish','potter', 'watch', 'movie', 'action', 'disappoitment']
+
+
+#example\
+totalraces = 3210
+avgac = 94.32
+avgsp = 77.82
+
+
+
+# database is index with columns : [text;totalraceswithtext; avgaccuracy for text; avg speed for text]
+#calculating scores
+scores = []
+for i in database:
+    score = 0
+    for j in mistakes:
+        score += (totalraces - i[1])/totalraces + i[2]/avgac + i[3]/avgsp +tf(j, tokenpara(i[0]))
+    scores.append(score)
+tid = scores.index(max(scores))
+print("choosing index para at :" + tid)
